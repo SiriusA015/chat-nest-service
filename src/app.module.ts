f07c6c7618a09dac07ca2da28d00f62b3f86fd29
Bzzard <sirius.a015@gmail.com>
2022-12-13 23:38:59 +0900

    feat: update module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AppService } from './app.service';
         useFindAndModify: false,
       },
     ),
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
